a1c19dbfbab2bcc1ff988272e52229e839cc1576
Dolphman <therealdolphman@gmail.com>
2015-09-04 12:43:52 -0400

    Added bs4parser.py, Will handle all xml parsing soon

---
+++ nationstates/NSback/bs4parser.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+
+def arraychild(xml, name):
+    return len(xml.findAll(name, recursive=False)) > 1
+
+
+def xml2dict(xml):
+    content = {"content": xml.text if xml.text else None}
+    atr = xml.attrs
+    if atr:
+        content.update(atr)
+    return {xml.name: xml}
+
+
+def readtree(xml, secondlevel=False):
+    masterdict = {}
+    children = xml.findChildren(recursive=False)
+    if not children:
+        return xml2dict(xml)
+    for child in children:
+        if arraychild(xml, child.name) or masterdict.get(child.name, False):
+            masterdict.update(
+                {child.name: [readtree(x) for x in xml.findAll(child.name, recursive=True)]})
+        elif child.findChildren(recursive=False):
+            masterdict.update({child.name: readtree(child, True)})
+        else:
+            masterdict.update(xml2dict(child))
+    return masterdict
+
+
+def parsetree(xml):
+    masterdict = {}
+    masterdict.update(readtree(xml))
+    return masterdict
