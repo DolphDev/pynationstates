a4d16c11c937ad54f3efd34303c036891bec8dff
Dolphman <therealdolphman@gmail.com>
2015-10-01 13:44:48 -0400

    0.15 update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 setup(name='nationstates',
       install_requires=["requests", "Beautifulsoup4", "xmltodict"],
-      version='0.143',
+      version='0.15',
       description='Nationstates API wrapper for python',
       author='Joshua Walters',
       author_email='therealdolphman@gmail.com',
