ed8189b9e9c68631333ecd2608c6e7c7db750b28
Dolphman <therealdolphman@gmail.com>
2015-10-14 18:06:39 -0400

    Version 0.22 release

--- setup.py
+++ setup.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python
+"""
+
+   ___       __     __                 
+  / _ \___  / /__  / /  __ _  ___ ____ 
+ / // / _ \/ / _ \/ _ \/  ' \/ _ `/ _ \
+/____/\___/_/ .__/_//_/_/_/_/\_,_/_//_/
+           /_/                         
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Dolphman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+"""
 
 from setuptools import setup
 setup(name='nationstates',
       install_requires=["requests", "Beautifulsoup4", "xmltodict"],
-      version='0.21',
+      version='0.22',
       description='Nationstates API wrapper for python',
       author='Joshua Walters',
       author_email='therealdolphman@gmail.com',
       url='https://github.com/Dolphman/pynationstates',
       packages=['nationstates', 'nationstates.NScore'],
-      download_url='https://github.com/Dolphman/pynationstates/tarball/0.21',
+      download_url='https://github.com/Dolphman/pynationstates/tarball/0.22',
       keywords=['nationstates', 'api wrapper', 'api',
                 "Nationstates API", "Nationstates API Wrapper", "Wrapper"],
       classifiers=["License :: OSI Approved :: MIT License",
