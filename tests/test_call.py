ba36d941909eacd41914d85c11ccbdea9900d76b
Dolphman <therealdolphman@gmail.com>
2015-10-27 14:17:28 -0400

    fixed test

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,18 +1,19 @@
 import unittest
 import nationstates as ns
 
-USERAGENT = "Automated Testing Builds by Travis CL for the nationstates API wrapper by Dolphman"
+USERAGENT = "Automated Testing Builds by Travis CL for the nationstates API wrapper by Dolphman."
 
 
-mycall = ns.Api("nation", "The United Island Tribes")
-mycall.load(USERAGENT)
+
 
 
 class CallTest(unittest.TestCase):
 
     def test_live_change(self):
         try:
-            mycall = ns.Nationstates("region", "balder")
+            mycall = ns.Nationstates("nation", "The United Island Tribes")
+            mycall.load(USERAGENT)
+            mycall("region", "balder")
             mycall.load(USERAGENT)
         except Exception as Err:
             self.fail(Err)
