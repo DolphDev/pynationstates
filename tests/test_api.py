904e9a614f96440ff6c20107b64be2703071035a
Dolphman <therealdolphman@gmail.com>
2015-12-31 13:14:47 -0500

    Added tests for Api Object

--- tests/test_api.py
+++ tests/test_api.py
@@ -29,3 +29,19 @@ class nationstates_api_object(unittest.TestCase):
         self.assertNotEqual(first_instance.api_instance, second_instance.api_instance)
         self.assertNotEqual(first_instance.api_instance, third_instnace.api_instance)
 
+    def test_api_user_agent(self):
+        api = ns.Api("Automated Testing")
+        api.user_agent = "New user_agent"
+        first_instance = api.get_nation("TEST", auto_load=False)
+        second_instance = api.get_region("TEST", auto_load=False)
+        third_instnace = api.get_wa("1", auto_load=False)
+        self.assertEqual(first_instance.user_agent, api.user_agent)
+        self.assertEqual(second_instance.user_agent, api.user_agent)
+        self.assertEqual(third_instnace.user_agent, api.user_agent)
+
+    def test_api_nscore_user_agent(self):
+        api = ns.Api("Automated Testing")
+        api.user_agent = "New user_agent"
+        instance = api.get_nation("TEST", auto_load=False)
+        self.assertEqual(api.user_agent, instance.user_agent, instance.api_instance.user_agent)
+
