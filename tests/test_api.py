f514b37d43b7631105c3a297a620fba032b33339
Dolphman <therealdolphman@gmail.com>
2016-01-02 18:50:17 -0500

    Added Tests for api.req_auth. fixed somes test to include api.get_world()

--- tests/test_api.py
+++ tests/test_api.py
@@ -35,13 +35,27 @@ class nationstates_api_object(unittest.TestCase):
         first_instance = api.get_nation("TEST", auto_load=False)
         second_instance = api.get_region("TEST", auto_load=False)
         third_instnace = api.get_wa("1", auto_load=False)
+        fourth_instance = api.get_world(shard=["test"], auto_load=False)
         self.assertEqual(first_instance.user_agent, api.user_agent)
         self.assertEqual(second_instance.user_agent, api.user_agent)
         self.assertEqual(third_instnace.user_agent, api.user_agent)
+        self.assertEqual(fourth_instance.user_agent, api.user_agent)
 
     def test_api_nscore_user_agent(self):
         api = ns.Api("Automated Testing")
         api.user_agent = "New user_agent"
         instance = api.get_nation("TEST", auto_load=False)
         self.assertEqual(api.user_agent, instance.user_agent, instance.api_instance.user_agent)
+
+    def test_api_isinstance_Nationstates(self):
+        api = ns.Api("Automated Testing")
+        self.assertIsInstance(api.get_nation("TEST", auto_load=False), ns.Nationstates)
+        self.assertIsInstance(api.get_region("TEST", auto_load=False), ns.Nationstates)
+        self.assertIsInstance(api.get_world(shard=["test"], auto_load=False), ns.Nationstates)
+        self.assertIsInstance(api.get_wa("TEST", auto_load=False), ns.Nationstates)
+        self.assertIsInstance(api.req_auth("TEST", checksum="TEST", auto_load=False), ns.AuthNationstates)
+  
+
+
+
 
