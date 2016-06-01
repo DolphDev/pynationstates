f40da4e41a3a7ab259f0a9792105382b05269615
Dolphman <therealdolphman@gmail.com>
2016-05-31 21:14:48 -0400

    Readded Removed Code

--- nationstates/NScore/__init__.py
+++ nationstates/NScore/__init__.py
@@ -164,6 +164,7 @@ class RequestMixin(ParserMixin):
                        " Retry-After: {seconds}".format(
                            seconds=(data["request_instance"]
                                     .headers["Retry-After"])))
+            raise APIRateLimitBan(message)
         if data["status"] == 500:
             message = ("Nationstates API has returned a Internal Server Error")
             raise APIError(message)
