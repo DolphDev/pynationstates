7a5605650c897d74bc64f7fc15d1c06642042c97
DolphDev <dolphdevgithub@gmail.com>
2019-01-16 17:16:18 -0500

    Added More tests

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,20 +1,45 @@
-# import unittest
-# import nationstates as ns
+import unittest
+import nationstates as ns
 
-# USERAGENT = "Automated Testing Builds by Travis CL for the nationstates API wrapper by The United Island Tribes."
-
-
+USERAGENT = "Automated Testing Builds by Travis CL for the nationstates API wrapper by The United Island Tribes."
 
 
+class CallTest(unittest.TestCase):
 
-# class CallTest(unittest.TestCase):
+    def test_nation_call(self):
+        try:
+            api = ns.Nationstates(USERAGENT)
 
-#     def test_live_change(self):
-#         try:
-#             api = ns.Nationstates(USERAGENT)
+            mycall = api.nation("testlandia")
+            mycall.get_shards("region")
+        except Exception as Err:
+            self.fail(Err)
 
-#             mycall = api.nation("testlandia")
-#             mycall.get_shards("region", "balder")
-#         except Exception as Err:
-#             self.fail(Err)
+    def test_region_call(self):
+        try:
+            api = ns.Nationstates(USERAGENT)
+
+            mycall = api.region("Balder")
+            mycall.get_shards("nations")
+        except Exception as Err:
+            self.fail(Err)
+
+    def test_world_call(self):
+        try:
+            api = ns.Nationstates(USERAGENT)
+
+            mycall = api.world()
+            mycall.get_shards("numnations")
+        except Exception as Err:
+            self.fail(Err)
+
+
+    def test_wa_call(self):
+        try:
+            api = ns.Nationstates(USERAGENT)
+
+            mycall = api.wa("1")
+            mycall.get_shards("numnations")
+        except Exception as Err:
+            self.fail(Err)
 
