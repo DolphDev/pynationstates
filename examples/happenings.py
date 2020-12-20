0b3f90b95ec4aca204f4e1a3d5fe80322d39cf4f
DolphDev <dolphdevgithub@gmail.com>
2020-12-20 14:32:59 -0500

    3.0.0.5 Release Commit

--- examples/happenings.py
+++ examples/happenings.py
@@ -4,5 +4,15 @@ from pprint import pprint
 
 api = nationstates.Nationstates("Example for Nationstates API for python")
 
+# get_shards
 resp = api.world().get_shards(Shard("happenings", view="region.the_pacific", filter='founding+move+cte', limit="5"))
 pprint(resp.happenings)
+
+# get_{{shard_name}}
+resp = api.world().get_happenings(view="region.the_pacific", filter='founding+move+cte', limit="5")
+pprint(resp.happenings)
+
+# attribute (can't pass arguments)
+# Automatically key access the shard for you
+resp = api.world().happenings
+pprint(resp)
