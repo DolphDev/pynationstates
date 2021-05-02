e2ad5c281d1113d8fa9d35d096caadd68fdb0e08
DolphDev <dolphdevgithub@gmail.com>
2021-05-02 19:45:55 -0400

    3.0.2.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 The MIT License (MIT)
 
-Copyright (c) 2015-2020 DolphDev
+Copyright (c) 2015-2021 DolphDev
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@ SOFTWARE.
 """
 
 
-version = '3.0.2.7'
+version = '3.0.2.8'
 
 from setuptools import setup
 setup(name='nationstates',
